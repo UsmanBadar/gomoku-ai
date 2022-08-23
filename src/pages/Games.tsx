317b4303cfece191266036e8ebaaa7080c3bc6b4
UsmanBadar <usman.badar006@gmail.com>
2022-08-23 20:11:55 +1000

    "Modified GameLog component"

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,38 +1,29 @@
-import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, {useContext} from 'react';
+import { UserContext } from '../context';
+import {useNavigate, Navigate} from 'react-router-dom';
+import {useLocalStorage} from '../hooks';
+import {GameState} from '../types';
+import style from './Games.module.css';
 
-type Move = {
-    player: string;
-    turn: number;
-  }
-  
-  type gameState = {
-    virtualBoard: Move [][];
-    result: string;
-}
-
-type localStorage ={
-    userGames:Record<string, gameState>;
-    setUserGames: any
-  }
-
-export default function Games(props:localStorage) {
-    const navigate = useNavigate();
-    const {userGames, } = props;
-    const keys = Object.keys(userGames)
-    console.log(keys);
-
+export default function Games() {
+  const {user} = useContext(UserContext);
+  const [userGames, ] = useLocalStorage<Record<string, GameState>>("UserGames",{});
+  const navigate = useNavigate();
+  const keys = Object.keys(userGames)
+  console.log(keys);
+  if(!user){return <Navigate to='/login' replace />}
     
-
-
-  return (
-    <>
-    {keys.map((key, index)=>
-        <div key={key}  onClick={() => navigate(`/game-log/${key}`)}>
-            Game #{key} Result: {userGames[key]['result']}
-        </div>
-     
-    )}
-    </>
-  )
+  if(keys.length > 0){
+    return (
+      <>
+      {keys.map((key, index)=>
+          <div className={style.gameLog} key={key} >
+             <p className={style.gameLogMsg}>{`Game # ${key} @ ${userGames[key]['date']} Result: ${userGames[key]['result']}`}</p>
+             <button className={style.gameLogButton} onClick={() => navigate(`/game-log/${key}`)}>View game log</button>
+          </div>
+      )}
+      </>
+      )}else{
+        return <div><p>There are no games in history</p></div>
+      }
 }
