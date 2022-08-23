7a13eeb6f0ee8fa5a459396a12d0e763e707198e
UsmanBadar <usman.badar006@gmail.com>
2022-08-23 20:06:41 +1000

    "Added route to handle undefined routes"

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
+import {Header, Login, Home, Game, Games, GameLog} from './pages';
+import {UserProvider} from './components';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
-function App() {
+import style from './App.module.css';
+
+
+
+
+export default function App() {
+  const [gameKey, setGameKey] = useState(1);
   return (
-    <>
-    </>
+    <UserProvider>
+        <Header/>
+        <main className={style.main}>
+          <Routes>
+            <Route path="/" element = {<Home/>} />
+            <Route path = "login" element = {<Login/>} />
+            <Route path = "game" element = {<Game key = {gameKey} gameKey={gameKey} setGameKey={setGameKey} />} />
+            <Route path = "games" element = {<Games/>} />
+            <Route path = "game-log/:id" element = {<GameLog/>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+    </UserProvider>
   );
 }
 
-export default App;
 
