dee5cb4c8dfbec59ddc14576ecce0a26e02e1b5e
UsmanBadar <usman.badar006@gmail.com>
2022-08-23 15:37:47 +1000

    "Added modified Home page"

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,44 +3,37 @@ import {useNavigate} from 'react-router-dom';
 import style from './Home.module.css';
 import {Input, Button, Message} from '../components';
 import {UserContext} from '../context';
-import {useLocation} from 'react-router-dom';
+
 
 export default function Navbar() {
   const [width, setWidth] = useState<number>(0);
   const [error, setError] = useState<boolean>(false);
-  const location = useLocation();
+  const {user, getBoardWidth} = useContext(UserContext);
   const navigate = useNavigate();
 
-  const {user, getBoardWidth} = useContext(UserContext);
-
   const handleFormSubmit = ()=>{
-    if(user === 'awp'){
+    if(user === 'admin'){
       if (width >= 5 && width <= 19){
         getBoardWidth(width);
         setError(false);
-        navigate('game');
+        navigate('/game');
       }else{
         setError(true);
       }
     }else{
-      navigate('login');
-    }
+      navigate('/login');
+    } 
   }
   return (
-    <>
-     <nav className = {style.nav}>
-      <h1 className={style.header}>Gomoku</h1>
-        <ul className={style.navList}>
-          {!user  && <button className={style.navListItem}>Log In</button>}
-         {location.pathname === '/' && <button className= {style.navListItem}>Previous Games</button>}
-        </ul>
-      </nav>
+    <div className={style.home}>
+      <div className = {style.message}>
       {error && <Message variant='error' message="Enter a number between 5 and 19" />}
-      <form onSubmit = {(e)=>{e.preventDefault(); handleFormSubmit()}}>
-        <Input type="text" onChange = {(e)=>setWidth(parseInt(e.target.value))}/>
+      </div>
+      <form className= {style.homeForm} onSubmit = {(e)=>{e.preventDefault(); handleFormSubmit()}}>
+        <Input type="text" placeholder= "Enter a number between 5 and 19" onChange = {(e)=>{setWidth(parseInt(e.target.value)); setError(false)}}/>
         <Button type = "submit">Submit </Button>
       </form>
-    </>
+    </div>
   );
 }
 
